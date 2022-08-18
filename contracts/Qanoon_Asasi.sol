968be679405b8525677d5c556b2839dac44d2c2a
w3villa-asad <asad.akhtar@w3villa.com>
2022-08-18 10:07:33 +0530

    qanoon asasi changes

--- contracts/Qanoon_Asasi.sol
+++ contracts/Qanoon_Asasi.sol
@@ -7,23 +7,57 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract QanoonAsasi is ERC20, Ownable{
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract QanoonAsasi is ERC20, Ownable, ReentrancyGuard{
     using SafeMath for uint256;
     address public _admin;
     
     // timestamps mapping
     mapping (address => uint256) public _timestamps;
+
+    uint256 public distributed;
+
+        // contract balance
+    uint256 public balance;
     
     // initial supply of tokens is 1 million and maximum supply is 100 million
-    uint256 public _initialSupply = 1_000_000 ether;
-    uint256 public _maxSupply = 100_000_000 ether;
+    uint256 public _initialSupply = 1_00_000 ether;
+    // uint256 public _maxSupply = 100_000_000 ether;
+
+    uint256 public salePriceUsd = 1000000000000; //$0.000001
     
     // admin mapping
     mapping (address => bool) public _isAdmin;
+
+    mapping (address => uint256) public _toClaim;
+
+    mapping (address => uint256) public  _ethDeposit;
+
+
+    mapping(address => uint256) public toRefund;
+
+    AggregatorV3Interface internal ethPriceFeed;
+
+
+/* EVENTS */
+    event Bought(address account, uint256 amount);
+    event Locked(address account, uint256 amount);
+    event Released(address account, uint256 amount);
+
+    event Buy(address indexed from, uint256 amount);
+    event Destroyed(uint256 burnedFunds);
+    event Transferred(address indexed to, uint256 amount);
+
+    event withdrawnETHDeposit(address indexed to, uint256 amount);
     
     constructor() ERC20('QanoonAsasi', 'QAN') {
         _mint(msg.sender, _initialSupply);
         _admin = msg.sender;
+        ethPriceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
     }
     
     // function mint(address to, uint amount) external onlyOwner{
@@ -31,9 +65,11 @@ contract QanoonAsasi is ERC20, Ownable{
     //     _mint(to, amount);
     // }
     
+    
+
     function mint(address _account, uint256 amount) public onlyOwner {
         // should not reach max supply
-        require(totalSupply().add(amount) <= _maxSupply, "ERC20: cap exceeded");
+        require(totalSupply().add(amount) <= _initialSupply, "ERC20: cap exceeded");
     
         _mint(_account, amount);
     }
@@ -53,9 +89,75 @@ contract QanoonAsasi is ERC20, Ownable{
     function removeAdmin(address _account) public onlyOwner {
         _isAdmin[_account] = false;
     }
+
+    function salePriceEth() public view returns (uint256) {
+            (, int256 ethPriceUsd, , , ) = ethPriceFeed.latestRoundData();
+            uint256 rbthlpriceInEth = (salePriceUsd.mul(10**18)).div(uint256(ethPriceUsd).mul(10**10));
+
+            return rbthlpriceInEth;
+    }   
+
+    function computeTokensAmount(uint256 funds) public view returns (uint256, uint256) {
+        uint256 salePrice = salePriceEth();
+        uint256 tokensToBuy = (funds.div(salePrice)).mul(10**18); // 0.5 6.5 = 6
+
+        uint256 exceedingEther;
+        
+        return (tokensToBuy, exceedingEther);
+    }
     
     function burn(uint amount) public {
         _burn(msg.sender, amount);
     }
+
+    function refundETH() external nonReentrant {
+    
+        require(_ethDeposit[msg.sender] > 0, "No ETH deposit to withdraw");
+
+        payable(msg.sender).transfer(_ethDeposit[msg.sender]);
+        
+        balance = balance.sub(_ethDeposit[msg.sender]);
+        
+        _ethDeposit[msg.sender] = 0;
+        
+        emit withdrawnETHDeposit(msg.sender, _ethDeposit[msg.sender]);
+
+    }
+
+
+        // users can claim rbthl tokens
+    function claim() external {
+        require(_ethDeposit[msg.sender] > 0, "No ETH deposit to claim");
+
+        transfer(msg.sender, _toClaim[msg.sender]);
+    }
+
+    function buy() public payable nonReentrant {
+
+        require(totalSupply() > 0, "everything was sold");
+
+        // compute the amount of token to buy based on the current rate
+        (uint256 tokensToBuy, uint256 exceedingEther) = computeTokensAmount(
+            msg.value
+        );
+        _toClaim[msg.sender] = _toClaim[msg.sender].add(tokensToBuy);
+
+
+        balance += msg.value;   // add the funds to the balance
+
+        // refund eventually exceeding eth
+        if (exceedingEther > 0) {
+            uint256 _toRefund = toRefund[msg.sender] + exceedingEther;
+            toRefund[msg.sender] = _toRefund;
+        }
+        distributed = distributed.add(tokensToBuy);
+
+        // Mint new tokens for each submission
+
+        // eth deposit of user is stored in _ethDeposit
+        _ethDeposit[msg.sender] = _ethDeposit[msg.sender].add(msg.value);
+
+        emit Buy(msg.sender, tokensToBuy);
+    }  
     
 }
